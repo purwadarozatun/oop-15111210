76971498a6a8535ab8040e146d0e25b8d714d15c
Purwa Darozatun <purwadarozatunakis@gmail.com>
2017-10-14 18:34:37 +0700

    Tugas 14-10-2017

---
+++ InputOutput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class InputOutput{
+	public static void main(String arg[]){
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		System.out.println("Output : " +  input);
+	}
+}
---
+++ Print.java
@@ -0,0 +1,5 @@
+class Print{
+	public static void main(String arg[]){
+		System.out.println("Hello World");	
+	}
+}
