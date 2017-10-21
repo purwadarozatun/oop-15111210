6786c59234a142e0c0d430bafedf76768fb20b71
Purwa Darozatun <purwadarozatunakis@gmail.com>
2017-10-21 17:42:54 +0700

    Pertemuan 21-10-2017

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,18 +1,18 @@
 class Mahasiswa{
-  public static void main (String arg[]){
-    System.out.println("INI Dari Parent");
-    Mahasiswa mhs = new Mahasiswa();
-    mhs.printNama();
-    System.out.println(mhs.calculateUmur());
-  }
+public static void main (String arg[]){
+  System.out.println("INI Dari Parent");
+  Mahasiswa mhs = new Mahasiswa();
+  mhs.printNama();
+  System.out.println(mhs.calculateUmur());
+}
 
-  public void printNama (){
-    System.out.println("Purwa Darozatun");
-  } 
+public void printNama (){
+  System.out.println("Purwa Darozatun");
+} 
 
-  public int calculateUmur() {
-    int tahunLahir = 1996;
-    int tahunSkarang = 2017;
-    return  tahunSkarang - tahunLahir;
-  }
+public int calculateUmur() {
+  int tahunLahir = 1996;
+  int tahunSkarang = 2017;
+  return  tahunSkarang - tahunLahir;
+}
 }
